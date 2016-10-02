9f4447d602178a1ff0f2f234f74c287031be1715
Sophia Ehlen <slehlen@wisc.edu>
2016-10-02 14:19:55 -0500

    spider follows each posting link and pulls attributes table.

--- craigslist/craigslist/spiders/sanfran.py
+++ craigslist/craigslist/spiders/sanfran.py
@@ -4,16 +4,24 @@ from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from craigslist.items import CraigslistItem
 
-class SanFranSpider(BaseSpider):
-       name = "craig"
-       allowed_domains = ["craigslist.org"]
-       start_urls = ["http://sfbay.craigslist.org/search/sfc/cto"]
+from scrapy import Spider, Request
 
-       def parse(self, response):
-               hxs = HtmlXPathSelector(response)
-               titles = hxs.select("//span[@class='pl']")
-               for titles in titles:
-                   item = CraigslistItem()
-                   title = titles.select("a/text()").extract()
-                   link = titles.select("a/@href").extract()
-                   print(title, link)
+class SanFranSpider(BaseSpider):
+    name = "craig"
+    allowed_domains = ["craigslist.org"]
+    start_urls = ["http://sfbay.craigslist.org/search/sfc/cto"]
+
+    BASE_URL = 'http://sfbay.craigslist.org/'
+
+
+    def parse(self, response):
+        links = response.xpath("//span[@class='pl']/a/@href").extract()
+        for link in links:
+            absolute_url = self.BASE_URL + link
+            yield scrapy.Request(absolute_url, callback=self.parse_attr)
+
+    def parse_attr(self, response):
+        item = CraigslistItem()
+        item["link"] = response.url
+        item["attr"] = "".join(response.xpath("//p[@class='attrgroup']//text()").extract())
+        return item
