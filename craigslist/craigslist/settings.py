e005169fcbf2c2dab319560d1df387ec0da66185
Sophia Ehlen <slehlen@wisc.edu>
2016-10-10 18:50:39 -0500

    for loop fun

--- craigslist/craigslist/settings.py
+++ craigslist/craigslist/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 4
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -72,12 +72,12 @@ DOWNLOAD_DELAY = 4
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
+#AUTOTHROTTLE_START_DELAY = 5.0
 # The maximum download delay to be set in case of high latencies
 #AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 2
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1
 # Enable showing throttling stats for every response received:
 AUTOTHROTTLE_DEBUG = True
 
--- craigslist/craigslist/spiders/sanfran.py
+++ craigslist/craigslist/spiders/sanfran.py
@@ -17,31 +17,47 @@ class SanFranSpider(BaseSpider):
     BASE_URL = 'http://sfbay.craigslist.org/'
 
     def start_requests(self):
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=100', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=200', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=300', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=400', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=500', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=600', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=700', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=800', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=900', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1000', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1100', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1200', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1300', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1400', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1500', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1600', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1700', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1800', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1900', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=2000', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=2100', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=2200', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=2300', self.parse)
-        yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=2400', self.parse)
+
+        for i in range(0, 12):
+            url_base = 'http://sfbay.craigslist.org/search/sfc/cto'
+            if i == 0:
+                yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto', self.parse)
+            else:
+                print(i)
+                sfc = '?s=' + str(100*i)
+                full_url = url_base + sfc
+                yield scrapy.Request(full_url, self.parse)
+        for z in range(12, 25):
+            print(z)
+            sfc = '?s=' + str(100*i)
+            full_url = url_base + sfc
+            yield scrapy.Request(full_url, self.parse)
+
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=100', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=200', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=300', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=400', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=500', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=600', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=700', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=800', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=900', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1000', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1100', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1200', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1300', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1400', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1500', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1600', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1700', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1800', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=1900', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=2000', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=2100', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=2200', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=2300', self.parse)
+        # yield scrapy.Request('http://sfbay.craigslist.org/search/sfc/cto?s=2400', self.parse)
 
     def parse(self, response):
         links = response.xpath("//span[@class='pl']/a/@href").extract()
